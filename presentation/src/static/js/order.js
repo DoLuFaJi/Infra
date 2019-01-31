04d49a35b3b77b7b08608e337b529f04c8925f2e
laetitia-lac <33545397+laetitia-lac@users.noreply.github.com>
2019-01-31 17:07:28 +0100

    Update order.js

--- presentation/src/static/js/order.js
+++ presentation/src/static/js/order.js
@@ -3,38 +3,6 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 // -- Bar Chart Example
-/**
-var ctx = document.getElementById("myBarChart");
-var myLineChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["February", "March", "April", "May", "June", "July"],
-    datasets: [{
-      label: "Revenue",
-      backgroundColor: "rgba(2,117,216,1)",
-      borderColor: "rgba(2,117,216,1)",
-      data: [5312, 6251, 7841, 9821, 14984, 0],
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
-        },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 6
-        }
-      }],
-    },
-    legend: {
-      display: false
-    }
-  }
-});**/
 
 Pusher.logToConsole = true;
 
@@ -43,17 +11,3 @@ var pusher = new Pusher('4adcbc6f4cdd9aed0a57', {
   cluster: 'eu',
   encrypted: true
 });
-
-/*
-// Subscribe to poll trigger
-var orderChannel = pusher.subscribe('order');
-
-// Listen to 'order placed' event
-var order = document.getElementById('order-count')
-orderChannel.bind('place', function(data) {
-  myLineChart.data.datasets.forEach((dataset) => {
-      dataset.data.fill(parseInt(data.units),-1);
-  });
-  myLineChart.update();
-  order.innerText = parseInt(order.innerText)+1
-});*/
